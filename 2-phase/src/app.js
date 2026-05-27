b7ada4cbcd83a98f047a6161e7204070b045b13b
Vishnu R <vishnu6364748848@gmail.com>
2026-05-27 22:56:30 +0530

    express setup in phase-2

--- 2-phase/src/app.js
+++ 2-phase/src/app.js
@@ -1,9 +1,18 @@
 import express from "express";
 
 const app = express();
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
+
+/**
+ * @requires POST /api/data
+ * @description get the data from the request body 
+ * @access public
+ */
+
+
 
 export default app;
