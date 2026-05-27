0d2e439b5b53dfdeee20f532bd05c5909555e363
Vishnu R <vishnu6364748848@gmail.com>
2026-05-27 19:31:37 +0530

    CRUD operations endpoints are created with easy to understand logic and comments

--- src/app.js
+++ src/app.js
@@ -10,11 +10,11 @@ app.get("/", (req, res) => {
 });
 
 /**
- * @Routes POST /api/new
+ * @Routes POST /api/notes
  * @description Create a new note and need title and description in the request body
  * @access Public
  */
-app.post("/api/new", async (req, res) => {
+app.post("/api/notes", async (req, res) => {
   const { title, description } = req.body;
 
   // ---- validation ----
@@ -66,6 +66,50 @@ app.get("/api/notes/:id", async (req, res) => {
   const { id } = req.params;
   const note = await NoteModel.findById(id);
   return res.status(200).json({ note });
+});
+
+/**
+ * @Routes PATCH /api/notes/:id
+ * @description Update a note by id
+ * @access Public
+ */
+app.patch("/api/notes/:id", async (req, res) => {
+  const { id } = req.params;
+  const { description } = req.body;
+
+  // ---- validation ----
+
+  if (!description) {
+    return res.status(400).json({ error: "Description required" });
+  }
+
+  if (description.trim().length < 10) {
+    return res
+      .status(400)
+      .json({ error: "description must be at least 10 characters long" });
+  }
+
+  // ---- if validation pass update the note ----
+
+  const note = await NoteModel.findById(id);
+
+  if (!note) {
+    return res.status(404).json({ message: "Note not found" });
+  }
+
+  note.description = description;
+  await note.save();
+
+  return res.status(200).json({
+    message: "Note updated successfully",
+    note
+  });
+});
+
+app.delete("/api/notes/:id", async (req, res) => {
+  const { id } = req.params;
+  await NoteModel.findByIdAndDelete(id);
+  return res.status(200).json({ message: "Note deleted successfully" });
 });
 
 export default app;
