e97cb611f32df0735ecca803f3f308e7299b5258
Vishnu R <vishnu6364748848@gmail.com>
2026-05-27 18:17:45 +0530

    conneted to mongodb and added gitIgnore

--- server.js
+++ server.js
@@ -1,4 +1,8 @@
 import app from "./src/app.js";
+import connectDB from "./src/config/db.js";
+
+connectDB();
+
 
 const PORT = 3000;
 
---
+++ src/config/db.js
@@ -0,0 +1,13 @@
+import monogoose from "mongoose";
+
+const connectDB = async () => {
+  try {
+    await monogoose.connect("mongodb://localhost:27017/3-times");
+    console.log("MongoDB connected successfully");
+  } catch (error) {
+    console.error("MongoDB connection failed:", error);
+    process.exit(1);
+  }
+}
+
+export default connectDB;
