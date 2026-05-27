c0ef0e5116cc4639694015cd39efcb632f0e3572
Vishnu R <vishnu6364748848@gmail.com>
2026-05-27 18:36:39 +0530

    created db schema and created our first test notes

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
 import express from "express";
+import NoteModel from "./models/notes.model.js";
 
 const app = express();
 
@@ -6,6 +7,23 @@ app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
+});
+
+app.post("/new", (req, res) => {
+  const { name, description } = req.body;
+  if (!name) {
+    return res.status(400).json({ message: "Name required" });
+  }
+  if (!description) {
+    return res.status(400).json({ message: "Description required" });
+  }
+
+  const note = NoteModel.create({ name, description });
+
+  res.status(201).json({
+    message: "Note created successfully",
+    note: { name, description },
+  });
 });
 
 export default app;
