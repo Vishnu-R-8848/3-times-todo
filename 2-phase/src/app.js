48d1bdeca0a6b35b9803767e07a38b253c8fca92
Vishnu R <vishnu6364748848@gmail.com>
2026-05-28 10:02:46 +0530

    implemented the update api in phase 2

--- 2-phase/src/app.js
+++ 2-phase/src/app.js
@@ -43,7 +43,7 @@ app.post("/api/notes", async (req, res) => {
   }
 
   // ---- if validation passes, create the note ---- //
-  const note =  await NoteModel.create({
+  const note = await NoteModel.create({
     title,
     description,
   });
@@ -62,11 +62,46 @@ app.post("/api/notes", async (req, res) => {
  */
 
 app.get("/api/notes", async (req, res) => {
-
   const notes = await NoteModel.find();
 
   return res.status(200).json({
     message: "Notes retrieved successfully",
+    notes,
+  });
+});
+
+app.patch("/api/notes/:id", async (req, res) => {
+  const { id } = req.params;
+  const { description } = req.body;
+
+  //---- Validation ----//
+
+  if (!description) {
+    return res.status(400).json({
+      message: "Description is required",
+    });
+  }
+
+  if (description.length < 10) {
+    return res.status(400).json({
+      message: "Description must be at least 10 characters long",
+    });
+  }
+
+  // ---- if validation passes, update the note ---- //
+  const notes = await NoteModel.findById(id);
+
+  if (!notes) {
+    return res.status(404).json({
+      message: "Note not found",
+    });
+  }
+
+  notes.description = description;
+  await notes.save();
+
+  return res.status(200).json({
+    message: "Note updated successfully",
     notes,
   });
 });
